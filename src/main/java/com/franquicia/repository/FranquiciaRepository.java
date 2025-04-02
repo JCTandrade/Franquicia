a47ddb0f91f2a35844db532ef98cc73dee7630d6
JCTandrade <142247971+JCTandrade@users.noreply.github.com>
2025-04-01 23:14:58 -0500

    Revert "Feature/sucursal"

--- src/main/java/com/franquicia/repository/FranquiciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.franquicia.repository;
-
-import com.franquicia.entities.FranquiciaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FranquiciaRepository extends JpaRepository<FranquiciaEntity, Long> {
-}
