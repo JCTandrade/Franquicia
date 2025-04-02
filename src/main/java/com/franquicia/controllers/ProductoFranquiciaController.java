1e44bd4a057de4f0f8a2a44116a2341610cc8a88
jctamayo <tamayojuan754@gmail.com>
2025-04-02 16:38:43 -0500

    Feat: Se agrega metodo para consultar producto

--- src/main/java/com/franquicia/controllers/ProductoFranquiciaController.java
+++ src/main/java/com/franquicia/controllers/ProductoFranquiciaController.java
@@ -27,14 +27,6 @@ public class ProductoFranquiciaController {
 
     @GetMapping("buscar-producto/{id}")
     @Operation(summary = "Método que permite obtener los producto", description = "Método que permite consultar los productos asociados a una franquicia en especifico.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Sucursal actualizada correctamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseGenerico.class), examples = @ExampleObject(name = "example1", value = "{\"status\":\"OK\",\"estadoOperacion\":\"SUCCESS\",\"message\":\"La operacion se realizado de manera correcta\",\"data\":{\"idSucursal\":1,\"nombre\":\"Actualizar sucursal desde postman 1232\",\"telefono\":\"00011133\"}}"))),
-            @ApiResponse(responseCode = "400", description = "La sucursal con el ID especificado y el ID del body no coincide.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseGenerico.class), examples = @ExampleObject(name = "example1", value = "{\"status\":\"BAD_REQUEST\",\"estadoOperacion\":\"ERROR\","
-                    +
-                    "\"message\":\"Error: la información suministrada no coincide.\",\"data\":null}"))
-
-            )
-    })
     ResponseEntity<ResponseGenerico> buscar(@PathVariable Long id) {
         ResponseGenerico servicio = this.iProductoFranquiciaService.obtenerSucursalesPorFranquicia(id);
         return new ResponseEntity<>(servicio, servicio.getStatus());
